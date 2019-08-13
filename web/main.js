8122a1f63a3d219fb3c6f8c965ef7bddb6cdee78
Zirialk <alejandro123888@hotmail.com>
2019-08-13 16:37:26 +0200

    main.js apunta la carpeta assets a dentro de la carpeta vendor

--- web/main.js
+++ web/main.js
@@ -11111,7 +11111,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function HttpLoaderFactory(http) {
-    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_22__["TranslateHttpLoader"](http, './assets/i18n/', '.json');
+    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_22__["TranslateHttpLoader"](http, '../vendor/olsf/core/web/assets/i18n/', '.json');
 }
 var initialRoutes = [
     {
